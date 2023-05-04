fe445c6297aa64e98530c564d4beb25b80465539
divyesh1111 <divyeshhirpara000@gmail.com>
2023-05-04 12:18:08 +0530

    initial commit 2

---
+++ frontend-next/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+        oswald: "Oswald, sans-serif",
+        urbanist: "Urbanist, sans-serif",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
