2626d6119e5a5ce7756ecaee2b7954e60472582e
Bless-10 <boradharsh29@gmail.com>
2023-05-04 18:05:50 +0530

    harsh

--- frontend-next/components/Header.jsx
+++ frontend-next/components/Header.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Header = () => {
-    return <div className="text">Harsh</div>;
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState("translate-y-0");
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [categories, setCategories] = useState(null);
+
+    return (
+        <div
+            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between
+             z-20 sticky top-0 transition-transform duration-300 ${show}`}
+        >
+            <Wrapper>
+                <Link href="/">
+                    <img src="/logo.svg" className="w-[40px] md:w-w[60px]" />
+                </Link>
+                <Menu />
+            </Wrapper>
+        </div>
+    );
 };
 
 export default Header;
---
+++ frontend-next/components/Menu.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Link from 'next/link';
+
+
+function Menu () {
+    const data = [
+        { id: 1, name: "Home", url: "/" },
+        { id: 2, name: "About", url: "/about" },
+        { id: 3, name: "Categories", subMenu: true },
+        { id: 4, name: "Contact", url: "/contact" },
+    ];
+
+    const subMenuData = [
+        { id: 1, name: "Jordan", doc_count: 11 },
+        { id: 2, name: "Sneakers", doc_count: 8 },
+        { id: 3, name: "Running shoes", doc_count: 64 },
+        { id: 4, name: "Football shoes", doc_count: 107 },
+    ];
+
+    return (
+        <ul className="hidden md:flex items-center gap-8  font-medium text-black">
+            {data.map((item) => {
+                return (
+                    <React.Fragment key={item.id}>
+                        {!!item?.subMenu ? 'submenu' : (
+                            <li className="cursor-pointer">
+                                <Link href={item?.url}>
+                                    {item.name}
+                                </Link>
+                            </li>
+                        )}
+                    </React.Fragment>
+                )
+            })}
+        </ul>
+    )
+}
+
+export default Menu
