c8055dfcae76d32781f4eae9700a0497a0a5da06
Bless-10 <boradharsh29@gmail.com>
2023-05-09 12:24:30 +0530

    harsh

--- frontend-next/components/ProductCard.jsx
+++ frontend-next/components/ProductCard.jsx
@@ -1,24 +1,39 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
 
-const ProductCard = () => {
+const ProductCard = ({ data: { attributes: p, id } }) => {
+
   return (
-    <Link href="/product/1" className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer ">
-        <img className="w-full" src="/product-1.webp" alt="Product image" />
-        <div className="p-4 text-black/[0.9] ">
-        <h2 className="text-lg font-medium">Product name</h2>
-            <div className="flex items-center text-black/[0.5]">
-                <p className="mr-2 text-lg font-semibold">₹8000</p>
-                <p className="text-base font-medium line-through ">
-                ₹9500
-                </p>
-                <p className="ml-auto text-base font-medium text-green-500">
-                    15% off
-                </p>
-            </div>
-        </div>          
-    </Link>
-  )
-}
+    <Link href={`/products/${p.slug}`}
+      className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer ">
 
-export default ProductCard
+
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data.attributes.url}
+        alt={p.name}
+      />
+
+      <div className="p-4 text-black/[0.9] ">
+        <h2 className="text-lg font-medium">{p.name}</h2>
+        <div className="flex items-center text-black/[0.5]">
+          <p className="mr-2 text-lg font-semibold">&#8377;{p.price}</p>
+          {p.original_price && (
+            <>
+              <p className="text-base font-medium line-through ">
+                &#8377;{p.original_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                15% off
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- frontend-next/pages/index.js
+++ frontend-next/pages/index.js
@@ -2,26 +2,21 @@ import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import { fetchDataFromApi } from "@/utils/api";
-import { useEffect, useState } from "react";
 
 
+export default function Home ({ products }) {
+    // traditional method of Java Script to fetch data but not efficient for SEO purpose
+    // const [data, setData] = useState(null);
 
+    // useEffect(() => { fetchProducts(); }, []);
+    // const fetchProducts = async () => {
 
+    // const { data } = await fetchDataFromApi('/api/products');
+    // setData(data);
 
-export default function Home () {
-
-    const [data, setData] = useState(null);
-
-    useEffect(() => { fetchProducts(); }, []);
-    const fetchProducts = async () => {
-
-        const { data } = await fetchDataFromApi('/api/products');
-        setData(data);
-
-    };
+    // };
 
     return (
-
         <main>
             <HeroBanner />
             <Wrapper>
@@ -31,30 +26,41 @@ export default function Home () {
                         Cushioning for Your Miles
                     </div>
                     <div className="text-md md:text-xl">
-                        A lightweight Nike ZoomX midsole is combined with
-                        increased stack heights to help provide cushioning
-                        during extended stretches of running.
+                        A lightweight Nike ZoomX midsole is combined with increased stack
+                        heights to help provide cushioning during extended stretches of
+                        running.
                     </div>
                 </div>
                 {/* heading and paragaph end */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+
+                    {products?.data?.map((product) => (
+
+                        <ProductCard key={product?.id} data={product} />
+                    ))}
+
+
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+                    // <ProductCard />
+
 
                 </div>
-
-
             </Wrapper>
-
-
-
         </main>
     );
+}
+
+
+export async function getStaticProps () {
+    const products = await fetchDataFromApi("/api/products?populate=*");
+
+    return {
+        props: { products },
+    };
 }
