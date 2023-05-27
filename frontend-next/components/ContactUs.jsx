f8713d4859c6446e2bbc078aaecbbcad2977f964
divyesh1111 <divyeshhirpara000@gmail.com>
2023-05-27 15:23:26 +0530

    d

---
+++ frontend-next/components/ContactUs.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Wrapper from "./Wrapper";
+
+
+const ContactUs = () => {
+    return (
+        <Wrapper>
+            <div className='text-center text-2xl md:text-[35px] font-bold py-4'> Contact Us</div>
+          <div className=" mx-auto px-6 py-6 h-[100%] w-[100%] flex grid-cols-2 md:grid-cols-4   my-8">
+            <div className="hello">
+                <div>
+                    
+                </div>
+            </div>
+            <div className="hello">hii hello</div>
+            <div className="hello">hii hello</div>
+            <div className="hello">hii hello</div>
+            
+          </div>
+        </Wrapper>
+      );
+    };
+
+export default ContactUs
