6343ca289b35c828b26e07721515529c6c722c7a
divyesh1111 <divyeshhirpara000@gmail.com>
2023-05-29 14:50:32 +0530

    div

--- frontend-next/pages/cart.js
+++ frontend-next/pages/cart.js
@@ -94,6 +94,7 @@ const Cart = () => {
                                 </div>
 
                                 {/* BUTTON START */}
+                                
                                 <button
                                     className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
                                     onClick={handlePayment}
