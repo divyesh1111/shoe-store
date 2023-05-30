7bbb82d4724c7c2a3bd76e3907afe4338a358d31
Bless-10 <boradharsh29@gmail.com>
2023-05-30 10:31:39 +0530

    harsh

--- frontend-next/components/Footer.jsx
+++ frontend-next/components/Footer.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import {FaFacebookF, FaTwitter, FaYoutube, FaInstagram} from "react-icons/fa";
 import Wrapper from "./Wrapper";
 import Link from "next/link";
-<<<<<<< Updated upstream
-=======
 import about2 from "./../pages/about2";
 import privacy from "@/pages/privacy";
->>>>>>> Stashed changes
 
 const Footer = () => {
   return (
@@ -60,17 +57,15 @@ const Footer = () => {
               <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
                 Payment Options
               </div>
-<<<<<<< Updated upstream
+
               <Link href="/contactUs">
                 <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
                   Contact Us
                 </div>
               </Link>
-=======
               <div className="text-sm text-white/[0.5] hover:text-white cursor-pointer">
                 <Link href={`contactUs`}>Contact Us</Link>
               </div>
->>>>>>> Stashed changes
             </div>
             {/* MENU END */}
 
--- frontend-next/pages/cart.js
+++ frontend-next/pages/cart.js
@@ -58,103 +58,14 @@ const Cart = () => {
               </div>
               {/* CART ITEMS END */}
 
-<<<<<<< Updated upstream
-                        {/* CART CONTENT START */}
-                        <div className="flex flex-col lg:flex-row gap-12 py-10">
-                            {/* CART ITEMS START */}
-                            <div className="flex-[2]">
-                                <div className="text-lg font-bold">
-                                    Cart Items
-                                </div>
-                                {cartItems.map((item) => (
-
-                                    <CartItem key={item.id} data={item} />
-                                ))}
-
-                            </div>
-                            {/* CART ITEMS END */}
-
-                            {/* SUMMARY START */}
-                            <div className="flex-[1]">
-                                <div className="text-lg font-bold">
-                                    Summary
-                                </div>
-                                <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
-                                    <div className="flex justify-between">
-                                        <div className="uppercase text-md md:text-lg font-medium text-black">
-                                            Subtotal
-                                        </div>
-                                        <div className="text-md md:text-lg font-medium text-black">
-                                            &#8377;{subTotal}
-                                        </div>
-                                    </div>
-                                    <div className="text-sm md:text-md py-5 border-t mt-5">
-                                        The subtotal reflects the total price of
-                                        your order, including duties and taxes,
-                                        before any applicable discounts. It does
-                                        not include delivery costs and
-                                        international transaction fees.
-                                    </div>
-                                </div>
-
-                                {/* BUTTON START */}
-                                
-                                <button
-                                    className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
-                                    onClick={handlePayment}
-                                >
-                                    Checkout
-                                    {loading && <img src="/spinner.svg" />}
-                                </button >
-                                <button
-                                    className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
-                                    
-                                >
-                                    <Link href={`guest`}>Guest </Link>
-                                    {loading && <img src="/spinner.svg" />}
-                                </button >
-                                {/* BUTTON END */}
-                            </div>
-                            {/* CART ITEMS END */}
-
-                        </div>
-                        {/* CART CONTENT START */}
-                    </>
-                )}
-
-                {/* This is empty screen */}
-                {cartItems.length < 1 &&
-                    <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
-                        <Image
-                            src="/empty-cart.jpg"
-                            width={300}
-                            height={300}
-                            className="w-[300px] md:w-[400px]"
-                            alt="empt-cart"
-                        />
-                        <span className="text-xl font-bold">
-                            Your cart is empty
-                        </span>
-                        <span className="text-center mt-4">
-                            Looks like you have not added anything in your cart.
-                            <br />
-                            Go ahead and explore top categories.
-                        </span>
-                        <Link
-                            href="/"
-                            className="py-4 px-8 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8"
-                        >
-                            Continue Shopping
-                        </Link>
-=======
               {/* SUMMARY START */}
               <div className="flex-[1]">
                 <div className="text-lg font-bold">Summary</div>
+
                 <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
                   <div className="flex justify-between">
                     <div className="uppercase text-md md:text-lg font-medium text-black">
                       Subtotal
->>>>>>> Stashed changes
                     </div>
                     <div className="text-md md:text-lg font-medium text-black">
                       &#8377;{subTotal}
@@ -178,11 +89,12 @@ const Cart = () => {
                 </button>
                 {/* BUTTON END */}
               </div>
-              {/* CART ITEMS END */}
+              {/* SUMMARY END */}
             </div>
-            {/* CART CONTENT START */}
+            {/* CART CONTENT END */}
           </>
         )}
+
         {/* This is empty screen */}
         {cartItems.length < 1 && (
           <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
@@ -191,8 +103,6 @@ const Cart = () => {
               width={300}
               height={300}
               className="w-[300px] md:w-[400px]"
-              alt="empt-cart"
-              priority={true}
             />
             <span className="text-xl font-bold">Your cart is empty</span>
             <span className="text-center mt-4">
@@ -208,7 +118,6 @@ const Cart = () => {
             </Link>
           </div>
         )}
-        ;
       </Wrapper>
     </div>
   );
