218b903595b9fea90a182c114c1adfec0ad75734
divyesh1111 <divyeshhirpara000@gmail.com>
2023-05-30 17:44:54 +0530

    d

--- frontend-next/components/TermsOfUse.jsx
+++ frontend-next/components/TermsOfUse.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Wrapper from './Wrapper'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
-const TermsOfUse = () => {
+const TermsOfUse = ({tou}) => {
   return (
     <Wrapper>
         <div className='text-center text-2xl md:text-[35px] font-bold py-4'> NIKE TERMS OF USE</div>
@@ -11,12 +12,16 @@ const TermsOfUse = () => {
         PLEASE READ THESE TERMS OF USE (“TERMS”) CAREFULLY BEFORE USING ANY NIKE PLATFORM.
         <br></br>
 
+        <ReactMarkdown>
+                                {tou?.tou.data?.attributes?.tou}
+                            </ReactMarkdown>
+{/* 
         <div className='mt-5'>If you live in any of the following countries or regions, additional terms may apply to you and are viewable at the bottom of these Terms. We display the country/region within the Terms when applicable.  These additional terms override the Terms below to the extent of any inconsistency.</div>
         <div className='mt-5'>
         Argentina, Australia, Brazil, Canada, Colombia, Hong Kong, Japan, Korea, Philippines, all European countries (including specific terms for Austria, Belgium, France, Germany, Hungary, Italy, Poland, and Switzerland).
-        </div>
+        </div> */}
 
-        <div className='mt-5'>
+        {/* <div className='mt-5'>
         Welcome to the NIKE community!  You are reading these Terms because you are using a NIKE website, digital experience, social media platform, mobile app, wearable technology, or one of our other products or services, all of which are part of NIKE’s Platform (“Platform”).  You may access the Platform through a computer, mobile phone, tablet, console, or other technology, which we refer to here as a “Device”.  Your carrier's normal rates and fees apply to your Device.
         </div>
         <div className='mt-5'>
@@ -33,13 +38,14 @@ const TermsOfUse = () => {
         <li className='mt-5'>
         Important Notice for Amateur Athletes.  You are responsible for ensuring that your participation on the Platform does not affect your eligibility as an amateur athlete.  Please check with your amateur athletic association for the rules that apply to you.  NIKE is not responsible or liable for your use of the Platform resulting in your ineligibility as an amateur athlete. 
         </li>
-        </ul>
+        </ul> */}
     
-
         </div>
       </div>
     </Wrapper>
   )
 }
+
+
 
 export default TermsOfUse
--- frontend-next/pages/termsOfUse.js
+++ frontend-next/pages/termsOfUse.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import TermsOfUse from '@/components/TermsOfUse'
+import { fetchDataFromApi } from '@/utils/api';
 
 
 
-const termsOfUse = () => {
+const termsOfUse = (tou) => {
   return (
     <div>
-        <TermsOfUse />
+        <TermsOfUse tou={tou} />
     </div>
   )
+}
+
+export async function getStaticProps(params) {
+  const tou = await fetchDataFromApi("/api/terms-of-use?populate=*");
+
+  return {
+    props: {tou},
+  };
 }
 
 export default termsOfUse
