a060a83d92540b5dc3ba6f068a97bef774f4b8c9
Bless-10 <boradharsh29@gmail.com>
2023-06-05 10:41:59 +0530

    harsh

--- frontend-next/pages/delivery.js
+++ frontend-next/pages/delivery.js
@@ -1,12 +1,34 @@
 import Delivery from "@/components/delivery";
 import React from "react";
 
-const delivery = () => {
+const delivery = ({products}) => {
   return (
     <div>
-      <Delivery />
+      <h1>Delivery Form</h1>
+      <Delivery products={products} />
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  try {
+    // Fetch necessary data from Strapi CMS API
+    const response = await fetch("https://your-strapi-api.com/products");
+    const products = await response.json();
+
+    return {
+      props: {
+        products,
+      },
+    };
+  } catch (error) {
+    console.log("Error:", error);
+    return {
+      props: {
+        products: [],
+      },
+    };
+  }
+}
 
 export default delivery;
